937ddac69b3b3465a7ccbb7ee30c1c59ba154537
Preethu Mary George <preethuedin@gmail.com>
2025-04-07 20:00:06 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Spaceman/",
+  base: "/Spaceman",
   test: {
     environment: "jsdom", // This ensures jsdom is used
   },
